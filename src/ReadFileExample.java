002ad7ab7d33bef41c9520c37e21d33c4347169e
VuKieuAnh <vukieuanh.hnue@gmail.com>
2020-03-04 16:08:56 +0700

    read file number.txt

---
+++ src/ReadFileExample.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class ReadFileExample {
+    public static void main(String[] args) throws IOException {
+        String filePath = "/Users/vuanh/IdeaProjects/APJ-DemoReadFile/src/numbers.txt";
+        File file = new File(filePath);
+
+        if (!file.exists()){
+            file.createNewFile();
+        }
+
+        FileInputStream fileInputStream = new FileInputStream(file);
+
+        int c = fileInputStream.read();
+        while (c != -1){
+            System.out.println(c);
+            c = fileInputStream.read();
+        }
+    }
+}
